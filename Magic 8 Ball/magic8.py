97d438bfc5e96cb1750d6a4ed72b5d621ce631f2
derryl0925 <116408020+derryl0925@users.noreply.github.com>
2023-09-18 22:57:24 -0700

    Add files via upload

--- Magic 8 Ball/magic8.py
+++ Magic 8 Ball/magic8.py
@@ -1,10 +1,12 @@
 import random
 
 name = "Derrick"
+#name = "" #to trigger alternative situation if user doesn't provide name
 question = "Will I learn python in a week?"
+#question = ""
 answer = " "
-random_number = random.randint(1, 9)
-print(random_number)
+random_number = random.randint(1, 12)
+print("Number:", random_number)
 if random_number == 1:
   answer = "Yes - definitely"
 elif random_number == 2:
@@ -23,8 +25,23 @@ elif random_number == 8:
   answer = "Outlook not so good"
 elif random_number == 9:
   answer = "Very doubtful"
+elif random_number == 10:
+  answer = "Batteries need to be replaced"
+elif random_number == 11:
+  answer = "Glitch, please try again"
+elif random_number == 12:
+  answer = "No - definitely"
 else:
   answer = "Error"
 
-print(name, "asks:", question)
-print("Magic 8-Ball's answer:", answer)
+if name == "" and question != "":
+  print("Question:", question)
+elif name == "" and question == "":
+  print("No question provided, cannot provide fortune")
+elif name != "" and question == "":
+  print("No question provided, cannot provide fortune")
+else:
+  print(name, "asks:", question)
+
+if question != "":
+  print("Magic 8-Ball's answer:", answer)
